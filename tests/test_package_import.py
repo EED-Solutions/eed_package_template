064a88a8a22bd661b8ab374c235d4406bc1f0765
EED85 <eric.brahmann@eed-solutions.de>
2025-07-15 07:49:43 +0200

    fix typo

--- tests/test_package_import.py
+++ tests/test_package_import.py
@@ -1,5 +1,5 @@
 def test_package_import():
-    import edd_package_template
+    import eed_package_template
 
-    print(edd_package_template.__path__)
+    print(eed_package_template.__path__)
     assert True
