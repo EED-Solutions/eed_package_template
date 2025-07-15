ad60c959d1a9e03bd422830c741afc81cd9ca3eb
EED85 <eric.brahmann@eed-solutions.de>
2025-07-15 08:21:25 +0200

    generalize test_package_import

--- tests/test_package_import.py
+++ tests/test_package_import.py
@@ -1,5 +1,17 @@
-def test_package_import():
-    import eed_package_template
+from pathlib import Path
+import importlib
 
-    print(eed_package_template.__path__)
+
+def test_package_import():
+    # Get the git root folder name
+    current_file = Path(__file__)
+    git_root = next(iter(current_file.parents))
+    while not (git_root / ".git").exists() and git_root != git_root.parent:
+        git_root = git_root.parent
+    package_name = git_root.name
+
+    # Import the package with the same name as the git root folder
+    package = importlib.import_module(package_name)
+
+    print(package.__path__)
     assert True
