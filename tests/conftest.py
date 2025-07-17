30a6ae1f7ec9f30418036f2accb4e666050ec81d
Eric Brahmann <37987769+EED85@users.noreply.github.com>
2025-07-17 08:01:15 +0200

    works locally (#5)

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,15 @@ from collections.abc import Iterator
 
 import pytest
 from pathlib import Path
+from eed_basic_utils.os import get_git_root
 
 
 @pytest.fixture(scope="session")
 def cfg_test() -> Iterator[dict]:
-    home_dir = str(Path.home())
+    home_dir = Path.home()
     cfg_test = {
         "home_dir": home_dir,
+        "cwd": Path.cwd(),
+        "git_root": get_git_root(),
     }
     yield cfg_test
--- tests/test_package_import.py
+++ tests/test_package_import.py
@@ -1,15 +1,9 @@
-from pathlib import Path
 import importlib
 
 
-def test_package_import():
-    # Get the git root folder name
-    current_file = Path(__file__)
-    git_root = next(iter(current_file.parents))
-    while not (git_root / ".git").exists() and git_root != git_root.parent:
-        git_root = git_root.parent
-    package_name = git_root.name
-
+def test_package_import(cfg_test):
+    """Test importing a package with the same name as the git root folder."""
+    package_name = cfg_test["git_root"].name
     # Import the package with the same name as the git root folder
     package = importlib.import_module(package_name)
 
